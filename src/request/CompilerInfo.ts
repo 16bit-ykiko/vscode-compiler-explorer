47d39e9d482d162460dd59f9d74759f6c6160604
張小凡 <2672931+whyb@users.noreply.github.com>
2024-05-21 18:15:25 +0800

    Apply proxy settings from vscode user configuration.

--- src/request/CompilerInfo.ts
+++ src/request/CompilerInfo.ts
@@ -1,3 +1,4 @@
+import * as vscode from "vscode";
 import axios from "axios";
 
 import { retry } from "./Utility";
@@ -21,10 +22,40 @@ export async function GetCompilerInfos() {
     if (compilerInfos.size === 0) {
         const fieldNames = Object.keys(new CompilerInfo()).join(",");
         const url = "https://godbolt.org/api/compilers/c++?fields=" + fieldNames;
+        const proxy = vscode.workspace.getConfiguration().get('http.proxy');
+        let axiosConfig = {};
+        if (proxy) {
+            const proxyParts = proxy.split('@');
+            if (proxyParts.length === 2) {
+                const [auth, proxyHost] = proxyParts;
+                const [username, password] = auth.split(':');
+                const [protocol, host] = proxyHost.split('://');
+                axiosConfig = {
+                    proxy: {
+                        protocol,
+                        host,
+                        port: parseInt(host.split(':')[1] || '80'),
+                        auth: {
+                            username,
+                            password,
+                        },
+                    },
+                };
+            } else {
+                const [protocol, host] = proxy.split('://');
+                axiosConfig = {
+                    proxy: {
+                        protocol,
+                        host,
+                        port: parseInt(host.split(':')[1] || '80'),
+                    },
+                };
+            }
+        }
 
         await retry("CompilerInfo", async () => {
             logger.info(`Request for CompilerInfo from ${url}`);
-            const response = await axios.get(url);
+            const response = await axios.get(url, axiosConfig);
             const infos = response.data as CompilerInfo[];
             infos.forEach((info) => {
                 compilerInfos.set(info.name, info);
